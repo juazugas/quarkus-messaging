4a17034686a394c1306793dd037fac1a7365c1dc
jzuriaga <jzuriaga@redhat.com>
2020-05-17 18:30:10 +0200

    Add Simple Server-Sent-Events request to the proxy.

---
+++ gateway/src/main/java/vlc/emergingtech/quarkus/gateway/SSEResource.java
@@ -0,0 +1,30 @@
+package vlc.emergingtech.quarkus.gateway;
+
+import java.time.Duration;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.SseElementType;
+import io.smallrye.mutiny.Multi;
+
+/**
+ * @see https://quarkus.io/guides/getting-started-reactive#handling-streams
+ */
+@Path("/sse")
+public class SSEResource {
+
+    @Path("/{count}")
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @SseElementType(MediaType.TEXT_PLAIN)
+    public Multi<String> methodname(@PathParam("count") int count, @QueryParam("name") String name) {
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(2))
+                    .onItem()
+                    .apply(n -> String.format("hello %s - %d", name, n))
+                    .transform().byTakingFirstItems(count);
+    }
+    
+}
